2af45ef817eacd368482db42400dbdc588aa719f
JunHoo Lee <mrjunoo@snu.ac.kr>
2026-04-07 18:59:19 +0900

    Widen public layout content area

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -8,8 +8,10 @@ export default function PublicLayout({
     return (
         <div className="flex flex-col md:flex-row min-h-screen">
             <Sidebar />
-            <main className="flex-1 md:ml-64 p-8 md:p-12 lg:p-20 max-w-4xl">
-                {children}
+            <main className="flex-1 md:ml-64 px-6 py-8 md:px-10 md:py-12 lg:px-14 lg:py-16">
+                <div className="mx-auto w-full max-w-6xl">
+                    {children}
+                </div>
             </main>
         </div>
     );
